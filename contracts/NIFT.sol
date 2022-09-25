07c227c170317607926a155336128dfc7df48875
Bhavishya Garg <bgarg@microsoft.com>
2022-09-25 23:29:32 +0530

    Deployment done

--- contracts/NIFT.sol
+++ contracts/NIFT.sol
@@ -1,13 +1,75 @@
 // SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+/// @title Base64
+/// @author Brecht Devos - <brecht@loopring.org>
+/// @notice Provides a function for encoding some bytes in base64
+library Base64 {
+    string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+        
+        // load the table into memory
+        string memory table = TABLE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+            
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+            
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+            
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+            
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               dataPtr := add(dataPtr, 3)
+               
+               // read 3 bytes
+               let input := mload(dataPtr)
+               
+               // write 4 characters
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
+               resultPtr := add(resultPtr, 1)
+            }
+            
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+        
+        return result;
+    }
+}
 
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Base64.sol";
+//import "./Base64.sol";
 
-contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
+contract NIFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
   
   struct Voucher {
@@ -23,7 +85,7 @@ contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
   
   mapping (uint256 => Voucher) public vouchers;
   
-  constructor() ERC721("Pretty Awesome Words", "PWA") {}
+  constructor() ERC721("NIFT Hack", "XNH") {}
 
   // public
   function mint(string memory _value, uint _amount, address payable _receiver) public payable {
@@ -35,7 +97,7 @@ contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
     }
     
     Voucher memory newWord = Voucher(
-        string(abi.encodePacked('PWA #', uint256(supply + 1).toString())), 
+        string(abi.encodePacked('XNH #', uint256(supply + 1).toString())), 
         "Test NFTs for Hack '22",
         randomNum(361, block.difficulty, supply).toString(),
         randomNum(361, block.timestamp, supply).toString(),
@@ -47,6 +109,7 @@ contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
      
     vouchers[supply + 1] = newWord;
     _safeMint(_receiver, supply + 1);
+    
   }
 
   function randomNum(uint256 _mod, uint256 _seed, uint _salt) public view returns(uint256) {
@@ -60,7 +123,7 @@ contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
           abi.encodePacked(
               '<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">',
               '<rect height="500" width="500" fill="hsl(',currentWord.bgHue,', 50%, 25%)"/>',
-              '<text x="50%" y="50%" dominant-baseline="middle" fill="hsl(',currentWord.textHue,', 100%, 80%)" text-anchor="middle" font-size="41">',currentWord.value,'</text>',
+              '<text x="50%" y="50%" dominant-baseline="middle" fill="hsl(',currentWord.textHue,', 100%, 80%)" text-anchor="middle" font-size="41">',currentWord.amount.toString(),'</text>',
               '</svg>'
           )
       ));
@@ -85,26 +148,13 @@ contract PrettyAwesomeWords is ERC721Enumerable, Ownable {
       return buildMetadata(_tokenId);
   }
 
-  /*function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }*/
 
-  function redeemVoucher(uint256 _tokenId) public {
+  function redeemVoucher(uint256 _tokenId) public payable{
         Voucher memory card = vouchers[_tokenId];
+        require(msg.sender == card.redeemer);
         require(card.isRedeemed == false , "Voucher is already redeemed");
         card.isRedeemed = true;
         card.redeemer.transfer(card.amount);
     }
 }
+
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  paths:{tests:"tests"},
+  paths: { tests:"tests"},
 };
 
 export default config;
